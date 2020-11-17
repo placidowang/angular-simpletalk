d4848f4f6c803780003a50010c77687caae81a35
Placido Wang <placidow@gmail.com>
2020-11-16 23:11:27 -0600

    Making first API call in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private _httpService: Http) { }
+  accessPointUrl: string = 'https://localhost:44234/api/values';
+  apiValues: string[] = [];
+
+  // makes initial API call when AppComponent is mounted
+  ngOnInit() {
+    this._httpService.get(this.accessPointUrl).subscribe(values => {
+      this.apiValues = values.json() as string[];
+    });  
+  }
+
   title = 'angular-simpletalk';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
